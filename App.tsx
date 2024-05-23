223c84baeb650e63f66dd804ddf56cf61ca6b70a
SangWon <kek2973@bizbee.co.kr>
2024-05-23 11:20:06 +0900

    Part Day2 [x] Add Icons to Bottom Tab Navigation

--- App.tsx
+++ App.tsx
@@ -3,6 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Text, View } from "react-native";
 import HomeScreen from "./src/screen/HomeScreen";
 
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
+
 const Tab = createBottomTabNavigator();
 
 function Home() {
@@ -16,11 +21,49 @@ function Home() {
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HOME" component={HomeScreen} />
-        <Tab.Screen name="REORDER" component={Home} />
-        <Tab.Screen name="CART" component={Home} />
-        <Tab.Screen name="ACCOUNT" component={Home} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#E96E6E",
+        }}
+      >
+        <Tab.Screen
+          name="HOME"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, focused, color }) => (
+              <Entypo name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="REORDER"
+          component={Home}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialIcons name="reorder" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="CART"
+          component={Home}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons name="cart" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ACCOUNT"
+          component={Home}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesome6 name="user" size={size} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
